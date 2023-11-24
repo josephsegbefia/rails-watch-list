45f63301fe187fd75012ca7c52242042807e3fc6
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-11-24 11:16:10 +0000

    Finish model and seed db

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,9 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true
+  validates :overview, presence: true
+
+  has_many :bookmarks, dependent: :restrict_with_error
+  has_many :lists, through: :bookmarks
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "open-uri"
+
+url = "http://tmdb.lewagon.com/movie/top_rated"
+top_rated_movies = JSON.parse(URI.open(url).read)
+
+puts "Destroying all existing movies in database"
+
+Movie.destroy_all
+
+puts "Entering the movie creation loop"
+top_rated_movies["results"].each do |movie|
+  Movie.create(title: movie["title"], overview: movie["overview"], rating: movie["vote_average"], poster_url: movie["poster_path"])
+end
+
+puts "Loop done, movies created"
